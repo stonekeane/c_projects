fe54942de9d76786605bf9234d754f2a86bff9c4
stonekeane <mrstoneethankeane@gmail.com>
2023-05-30 16:38:42 -0500

    started

---
+++ blink/main.c/main.c.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(7, OUTPUT); // initialize digital pin 7 as an output
+}
+
+void loop() {
+  digitalWrite(7, HIGH); //turn the LED on by making the voltage HIGH
+  delay(40); //wait for a half second
+  digitalWrite(7, LOW); //turn the LED off by making the voltage LOW
+  delay(40); //wait for a half second 
+}
